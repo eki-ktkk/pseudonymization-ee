1c36cd5aed2a0078e5802e1e8bab41c4f487d321
eduardo <eduardosanchezg37@gmail.com>
2021-03-26 16:12:07 +0100

    adding fastapi

---
+++ app.py
@@ -0,0 +1,22 @@
+# 1. Library imports
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+import mspresidio
+
+class TextModel(BaseModel):
+    text: str
+
+# 2. Create the app object
+app = FastAPI()
+
+
+@app.post('/anonymize', response_model=str,tags=['anonymize'])
+def tokenize(text : str):
+    anon = mspresidio.anonymize(text)
+    return anon
+
+# 5. Run the API with uvicorn
+#    Will run on http://127.0.0.1:8000
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
